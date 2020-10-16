782cbe8e7a1fe097d88d1f0b364d74bd4fed002e
UniquelySimilar <consciousexplorer@gmail.com>
2020-10-16 11:10:27 -0600

    customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,15 +7,26 @@ class CustomersController < ApplicationController
     render json: customers
   end
 
-  def create
+  def show
+    customer = Customer.find(params[:id])
+    render json: customer
   end
 
-  def show
+  def create
+    customer = Customer.create(customer_params)
   end
 
   def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
   end
 
   def destroy
+    customer = Customer.find(params[:id])
+    customer.destroy
+  end
+
+  def customer_params
+    params.permit(:first_name, :last_name, :street, :city, :state, :zipcode, :home_phone, :work_phone, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'customers#index'
   
-  get '/customers', to: 'customers#index'
-  get 'customers/create'
-  get 'customers/show'
-  get 'customers/update'
-  get 'customers/destroy'
+  get 'customers', to: 'customers#index'
+  get 'customers/:id', to: 'customers#show'
+  post 'customers', to: 'customers#create'
+  put 'customers/:id', to: 'customers#update'
+  delete 'customers/:id', to: 'customers#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
